27da84b00066ac0ea7f87edfcbcb8b014bcaf193
unknown <psychemistz@gmail.com>
2020-08-18 22:32:57 +0900

    apply tidy_dir in formatR package.

--- R/distfit.R
+++ R/distfit.R
@@ -1,21 +1,20 @@
-## Implemented by Seongyong Park (2020-08-18)
-## Overlap calculation by distribution fitting
+## Implemented by Seongyong Park (2020-08-18) Overlap calculation by distribution fitting
 
-ovl_distfit <- function(x1, x2, family=c("norm", "lognorm")){
-
-  x <- range(min(x1, x2), max(x1, x2))
-  len_x = min(length(x1), length(x2))
-  x <- seq(x[1], x[2], length.out=1+log2(len_x))
-
-  fit_t <- fitdist(x1, family)
-  fit_b <- fitdist(x2, family)
-
-  pdf_t = hist(x1, x, plot=FALSE)
-  pdf_b = hist(x2, x, plot=FALSE)
-  pd_t <- pnorm(x, fit_t$estimate[1], fit_t$estimate[2])
-  pd_b <- pnorm(x, fit_b$estimate[1], fit_b$estimate[2])
-
-  OVL <- sum(apply(cbind(diff(pd_b), diff(pd_t)), 1, min))
-
-  return (OVL)
+ovl_distfit <- function(x1, x2, family = c("norm", "lognorm")) {
+    
+    x <- range(min(x1, x2), max(x1, x2))
+    len_x = min(length(x1), length(x2))
+    x <- seq(x[1], x[2], length.out = 1 + log2(len_x))
+    
+    fit_t <- fitdist(x1, family)
+    fit_b <- fitdist(x2, family)
+    
+    pdf_t = hist(x1, x, plot = FALSE)
+    pdf_b = hist(x2, x, plot = FALSE)
+    pd_t <- pnorm(x, fit_t$estimate[1], fit_t$estimate[2])
+    pd_b <- pnorm(x, fit_b$estimate[1], fit_b$estimate[2])
+    
+    OVL <- sum(apply(cbind(diff(pd_b), diff(pd_t)), 1, min))
+    
+    return(OVL)
 }
--- R/hist.R
+++ R/hist.R
@@ -1,24 +1,22 @@
-## This code is adopted from GCNR paper. The original code was matlab but I
-## implemented it in the R. Reference: Rodriguez-Molares et. al, 2019. The generalized contrast-to-noise ratio:
-## a formal definition for lesion detectability. IEEE Transactions on Ultrasonics, Ferroelectrics, and Frequency Control
-## Codebase: No code base is available. please contact to <alfonso.r.molares@ntnu.no>
+## This code is adopted from GCNR paper. The original code was matlab but I implemented it in the R. Reference: Rodriguez-Molares et. al,
+## 2019. The generalized contrast-to-noise ratio: a formal definition for lesion detectability. IEEE Transactions on Ultrasonics,
+## Ferroelectrics, and Frequency Control Codebase: No code base is available. please contact to <alfonso.r.molares@ntnu.no>
 
-## reimplemented by Seongyong Park (2020-08-18)
-## The original implementation used fixed sized bin (n=100) but I used adaptive bin size selection method proposed in
-## C. J. Cellucci, A. M. Albano, and P. E. Rapp, “Statistical validation of mutual information calculations:
-## Comparison of alternative numerical algorithms,” Physical Review E, vol. 71, no. 6, p. 066208, 2005.
+## reimplemented by Seongyong Park (2020-08-18) The original implementation used fixed sized bin (n=100) but I used adaptive bin size
+## selection method proposed in C. J. Cellucci, A. M. Albano, and P. E. Rapp, <U+653C><U+3E32>\u0080�Statistical validation of mutual information
+## calculations: Comparison of alternative numerical algorithms,<U+653C><U+3E32>\u0080<U+393C><U+3E64> Physical Review E, vol. 71, no. 6, p. 066208, 2005.
 
-ovl_hist <- function(x1, x2){
-
-  x <- range(min(x1, x2), max(x1, x2))
-  len_x = min(length(x1), length(x2))
-  x <- seq(x[1], x[2], length.out=1+log2(len_x))
-
-  pdf_t = hist(x1, x, plot=FALSE)
-  pdf_b = hist(x2, x, plot=FALSE)
-
-  OVL <- sum(apply((rbind(pdf_t$counts/sum(pdf_t$counts), pdf_b$counts/sum(pdf_b$counts))),2,min))
-
-  return (OVL)
+ovl_hist <- function(x1, x2) {
+    
+    x <- range(min(x1, x2), max(x1, x2))
+    len_x = min(length(x1), length(x2))
+    x <- seq(x[1], x[2], length.out = 1 + log2(len_x))
+    
+    pdf_t = hist(x1, x, plot = FALSE)
+    pdf_b = hist(x2, x, plot = FALSE)
+    
+    OVL <- sum(apply((rbind(pdf_t$counts/sum(pdf_t$counts), pdf_b$counts/sum(pdf_b$counts))), 2, min))
+    
+    return(OVL)
 }
 
