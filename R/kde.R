77c8f33f649de2428ef66df6832ec827757284c6
unknown <psychemistz@gmail.com>
2020-08-18 22:39:45 +0900

    apply lint_package()

--- R/kde.R
+++ R/kde.R
@@ -6,21 +6,23 @@
 ## estimation (KDE).  The original package used base density function to perform KDE.
 
 ovl_kde <- function(x1, x2, n = 1024) {
-    
+
     get_density <- function(x, n = 1024, from = from, to = to) {
-        res = density(x, n = n, from = from, to = to)
+        res <- density(x, n = n, from = from, to = to)
         return(data.frame(x = res$x, y = res$y))
     }
-    
-    min_x = min(min(x1), min(x2))
-    max_x = max(max(x1), max(x2))
-    
-    d_y1 = get_density(x1, n = n, from = min_x, to = max_x)
-    d_y2 = get_density(x2, n = n, from = min_x, to = max_x)
-    
-    OVL = d_y1 %>% dplyr::mutate(y1 = y) %>% left_join(d_y2 %>% mutate(y2 = y), by = "x") %>% dplyr::select(x, y1, y2) %>% group_by(x) %>% 
-        summarise(OVL = min(y1, y2), OVA = max(y1, y2), .groups = "drop") %>% ungroup() %>% summarise(total = sum(OVL)/sum(OVA), .groups = "drop") %>% 
+
+    min_x <- min(min(x1), min(x2))
+    max_x <- max(max(x1), max(x2))
+
+    d_y1 <- get_density(x1, n = n, from = min_x, to = max_x)
+    d_y2 <- get_density(x2, n = n, from = min_x, to = max_x)
+
+    OVL <- d_y1 %>% dplyr::mutate(y1 = y) %>% left_join(d_y2 %>% mutate(y2 = y), by = "x") %>%
+        dplyr::select(x, y1, y2) %>% group_by(x) %>%
+        summarise(OVL = min(y1, y2), OVA = max(y1, y2), .groups = "drop") %>% ungroup() %>%
+        summarise(total = sum(OVL) / sum(OVA), .groups = "drop") %>%
         unlist(use.names = FALSE)
-    
+
     return(OVL)
 }
--- R/knn.R
+++ R/knn.R
@@ -5,58 +5,55 @@
 ## Modified by Seongyong Park (2020-08-18) Rvalue provides estimated overlap between two classes or more. Here I assumed only two classes.
 
 ovl_knn <- function(x1, x2, k = 5) {
-    
-    rvalue = 0
-    ds = cbind(c(x1, x2))  ## data matrix
-    cl = cbind(c(rep(0, length(x1)), rep(1, length(x2))))  ## class vector (column vector)
-    
-    noRow = nrow(ds)  # no of Row
-    knns = get.knn(ds, k)  # find k nearest neighbors for all instances (FNN package)
+
+    rvalue <- 0
+    ds <- cbind(c(x1, x2))  ## data matrix
+    cl <- cbind(c(rep(0, length(x1)), rep(1, length(x2))))  ## class vector (column vector)
+
+    noRow <- nrow(ds)  # no of Row
+    knns <- get.knn(ds, k)  # find k nearest neighbors for all instances (FNN package)
     knns.ndx <- data.frame(knns[["nn.index"]])
-    
-    inOverlap = c()
-    diffNeighbor = 0
+
+    diffNeighbor <- 0
     for (i in 1:noRow) {
-        thisClass = cl[i]
+        thisClass <- cl[i]
         for (j in 1:k) {
             if (cl[knns.ndx[i, j]] != thisClass) {
-                diffNeighbor = diffNeighbor + 1
+                diffNeighbor <- diffNeighbor + 1
             }
         }
     }
-    
-    rvalue = diffNeighbor/(noRow * k)
+
+    rvalue <- diffNeighbor / (noRow * k)
     return(rvalue)
 }
 
 ## Generalized version of Rvalue. Here ds denotes data or feature matrix and cl denotes class vector.
 Rvalue <- function(ds, cl, k = 5) {
-    rvalue = 0
+    rvalue <- 0
     if (is.vector(ds)) {
-        ds = cbind(ds)
+        ds <- cbind(ds)
     }
     if (is.vector(cl)) {
-        cl = cbind(cl)
+        cl <- cbind(cl)
     }
-    
-    noRow = nrow(ds)  # no of Row
-    
-    knns = get.knn(ds, k)  # find k nearest neighbors for all instances
+
+    noRow <- nrow(ds)  # no of Row
+
+    knns <- get.knn(ds, k)  # find k nearest neighbors for all instances
     knns.ndx <- data.frame(knns[["nn.index"]])
-    
-    inOverlap = c()
-    
-    diffNeighbor = 0
+
+    diffNeighbor <- 0
     for (i in 1:noRow) {
-        thisClass = cl[i]
+        thisClass <- cl[i]
         for (j in 1:k) {
             if (cl[knns.ndx[i, j]] != thisClass) {
-                diffNeighbor = diffNeighbor + 1
+                diffNeighbor <- diffNeighbor + 1
             }
         }
     }
-    
-    rvalue = diffNeighbor/(noRow * k)
+
+    rvalue <- diffNeighbor / (noRow * k)
     return(rvalue)
 }
 
