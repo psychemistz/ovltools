93e45d53ac343404942b1010dfa3b0e078df7a15
unknown <psychemistz@gmail.com>
2020-08-19 15:08:18 +0900

    updated manual and description files

---
+++ R/ovltools.R
@@ -0,0 +1,18 @@
+#' ovltools: A package for computating distribution overlap between two sample distributions
+#'
+#' The ovltools package provides four methods to compute distribution overlaps:
+#' KNN, KDE, histogram and distribution fitting.
+#'
+#' @section ovltools functions:
+#'
+#' ovl_knn: calculation by KNN
+#'
+#' ovl_kde: calculation by KDE
+#'
+#' ovl_hist: calculation by histogram
+#'
+#' ovl_distfit: calculation by distribution fitting
+#'
+#' @docType package
+#' @name ovltools
+NULL
